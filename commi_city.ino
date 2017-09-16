95983865e4f76f266c388c8cd181faae6eee4959
Mikekim0526 <kimminho0526@naver.com>
2017-09-16 12:29:56 +0900

    Update commi_city.ino

--- commi_city.ino
+++ commi_city.ino
@@ -8,10 +8,12 @@ LedControl lc=LedControl(12,11,10,1);
 
 unsigned long delaytime=500;
 
-byte D[8]={B00000000,B00000000,B00000000,B00011000,B00011000,B00000000,B00000000,B00000000};
-byte C[8]={B00000000,B00000000,B00111100,B00111100,B00111100,B00111100,B00000000,B00000000};
-byte B[8]={B00000000,B01111110,B01111110,B01111110,B01111110,B01111110,B01111110,B00000000};
-byte A[8]={B11111111,B11111111,B11111111,B11111111,B11111111,B11111111,B11111111,B11111111};
+byte A[8]={B00000000,B00000000,B00000000,B00011000,B00011000,B00000000,B00000000,B00000000};
+byte B[8]={B00000000,B00000000,B00111100,B00111100,B00111100,B00111100,B00000000,B00000000};
+byte C[8]={B00000000,B01111110,B01111110,B01111110,B01111110,B01111110,B01111110,B00000000};
+byte D[8]={B11111111,B11111111,B11111111,B11111111,B11111111,B11111111,B11111111,B11111111};
+byte E[8]={B11111111,B11111111,B11100011,B11010011,B11001011,B11000111,B11111111,B11111111};
+byte F[8]={B10000000,B01000000,B00111100,B00111100,B00111100,B00111100,B00000010,B00000001};
 
 void setup() {
   Serial.begin(9600);
@@ -26,14 +28,14 @@ void setup() {
 }
 
 void onestep(){
-  lc.setRow(0,3,A[0]);
-  lc.setRow(0,2,A[1]);  
-  lc.setRow(0,1,A[2]);
-  lc.setRow(0,0,A[3]);
-  lc.setRow(0,7,A[4]);
-  lc.setRow(0,6,A[5]);
-  lc.setRow(0,5,A[6]);
-  lc.setRow(0,4,A[7]);
+  lc.setRow(0,7,A[0]);
+  lc.setRow(0,6,A[1]);  
+  lc.setRow(0,5,A[2]);
+  lc.setRow(0,4,A[3]);
+  lc.setRow(0,3,A[4]);
+  lc.setRow(0,2,A[5]);
+  lc.setRow(0,1,A[6]);
+  lc.setRow(0,0,A[7]);
   delay(100);
 }
 void twostep(){
@@ -69,7 +71,26 @@ void fourstep(){
   lc.setRow(0,0,D[7]);
   delay(100);
 }
-
+void laststep(){
+  lc.setRow(0,7,E[0]);
+  lc.setRow(0,6,E[1]);
+  lc.setRow(0,5,E[2]);
+  lc.setRow(0,4,E[3]);
+  lc.setRow(0,3,E[4]);
+  lc.setRow(0,2,E[5]);
+  lc.setRow(0,1,E[6]);
+  lc.setRow(0,0,E[7]);
+  delay(300);
+  lc.setRow(0,7,F[0]);
+  lc.setRow(0,6,F[1]);
+  lc.setRow(0,5,F[2]);
+  lc.setRow(0,4,F[3]);
+  lc.setRow(0,3,F[4]);
+  lc.setRow(0,2,F[5]);
+  lc.setRow(0,1,F[6]);
+  lc.setRow(0,0,F[7]);
+  delay(300);
+}
 void loop() {
   long duration, distance;    
   digitalWrite(trigPin, LOW);  
@@ -84,18 +105,15 @@ void loop() {
     delay(10);
   }
 
-  if(val<100){
+  if(val>500){
     onestep();
-  } else if(val<200){
+  } else if(val>410){
     twostep();
-  } else if(val<300){
+  } else if(val>280){
     threestep();
-  } else if(val<400){
+  } else if(val>150){
     fourstep();
-  } else{
-    fourstep();
-    delay(300);
-    twostep();
-    delay(200);
+  } else {
+    laststep();
   }
 }
